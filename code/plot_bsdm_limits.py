dcf878051a2fa8fb0a8ffe62aeb3a4b39156c254
Alex Drlica-Wagner <kadrlica@fnal.gov>
2019-04-12 01:01:52 -0500

    bsdm limits

--- code/plot_bsdm_limits.py
+++ code/plot_bsdm_limits.py
@@ -22,10 +22,13 @@ plot_limit_patch(limits['emken2018_cresst3_dd'])
 plot_limit_patch(limits['erickcek2007_xqc'])
 #plot_limit_patch(limits['mahdawi2019_xqc_e1'])
 
-plot_limit_patch(limits['bringmann2018_xenon1t'])
-plot_limit_patch(limits['bringmann2018_miniboone'])
+#plot_limit_patch(limits['bringmann2018_xenon1t'])
+#plot_limit_patch(limits['bringmann2018_miniboone'])
 #plot_limit_patch(limits['bringmann2018_v2_xenon1t'])
 #plot_limit_patch(limits['bringmann2018_v2_miniboone'])
+
+plot_limit_patch(limits['bringmann2018_v3_xenon1t'])
+plot_limit_patch(limits['bringmann2018_v3_miniboone'])
 
 plt.xlim(1e-5,1e1)
 plt.ylim(1e-34,1e-24)
