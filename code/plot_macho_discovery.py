eae656b0b03530d798a6766d6f2e8f404f563844
Alex Drlica-Wagner <kadrlica@fnal.gov>
2019-04-14 18:31:34 -0500

    updated macho discovery plot

--- code/plot_macho_discovery.py
+++ code/plot_macho_discovery.py
@@ -14,53 +14,42 @@ import fitsio
 import lsstplot
 from lsstplot import COLORS
 
-np.random.seed(0)
+filename = lsstplot.get_datafile('pbh_popsycle_nocuts.fits')
+data = fitsio.read(filename)
 
-def draw_hist(*args, **kwargs):
-  kw = copy.deepcopy(kwargs)
-  lw = kw.pop('lw',2)
-  kw.update(histtype='stepfilled',lw=0)
-  plt.hist(*args,**kw)
-  kw.update(histtype='step',label=None,lw=lw)
-  kw.pop('label',None)
-  plt.hist(*args,**kw)
+bins = np.log10(data['bin_edge'])
+bin_width = np.unique(bins[1:] - bins[:-1])
+bins = np.append(bins, bins[-1]+bin_width)
+bins = 10**bins
 
-filename = "../data/OGLE_8_20_refined_events.fits"
-data = fitsio.read(filename,columns=['rem_id_L','t_E','mass_L'])
-data_BH = data[data['rem_id_L']==3]
+scale = 10*10 / 3.74 # scaling 3.74 deg^2 to (10 x 10deg)
 
-#bins = np.logspace(np.log10(np.min(data['t_E'])),np.log10(np.max(data['t_E'])),26)
-bins = np.logspace(-1,4,27)
+def plot_hist(x,y,**kwargs):
+  defaults = dict(histtype='step',lw=2)
+  label = kwargs.pop('label',None)
+  n,b,p = plt.hist(x,weights=y,**kwargs)
+  color = p[0].get_edgecolor()
+  plt.plot(np.nan,np.nan,lw=kwargs['lw'],color=color,label=label)
+  return n,b,p
 
-fig,ax = plt.subplots()
-kwargs = dict(lw=3,histtype='stepfilled',log=True,bins=bins)
+plt.figure()
+kwargs = dict(bins=bins,histtype='step',lw=2.5)
 
-draw_hist(data['t_E'],color='0.5',label=r'All Events',zorder=2,**kwargs)
-draw_hist(data_BH['t_E'],color=COLORS['red'],label=r'Astrophysical BHs',zorder=3,alpha=0.3,**kwargs)
+plot_hist(data['bin_edge'],scale*data['no_pbh'],label='No PBHs',
+          color='k',zorder=10,**kwargs)
+plot_hist(data['bin_edge'],scale*data['all_f 5'],label=r'$f_{PBH}=5\%$',
+          color='skyblue',**kwargs)
+plot_hist(data['bin_edge'],scale*data['all_f15'],label=r'$f_{PBH}=15\%$',
+          color=COLORS['blue'],**kwargs)
+plot_hist(data['bin_edge'],scale*data['all_f30'],label=r'$f_{PBH}=30\%$',
+          color='b',**kwargs)
 
-colors = ['green',COLORS['blue'],COLORS['orange']]
-fracs = [.05,.15,.30]
+plt.gca().set_xscale('log'); plt.gca().set_yscale('log')
+plt.xlim(1,6e3)
 
-for i,(color,frac) in enumerate(zip(colors,fracs)):
-  N = int(len(data_BH)*frac)
-  data_frac = copy.deepcopy(data)
+plt.legend(loc='upper right')
 
-  idx = np.random.choice(np.squeeze(np.where(data['rem_id_L']==3)),N,replace=False)
-  #data_frac['t_E'][idx] *= np.sqrt(data_frac['mass_L'][idx]*10)
-  data_frac['t_E'][idx] *= np.sqrt(10)
+plt.xlabel(r'$t_E$ (days)',fontsize=18)
+plt.ylabel('Number of Events',fontsize=18)
 
-  draw_hist(data_frac['t_E'],color=color,label=r'All Events + %d\%% PBHs'%(frac*100),zorder=1-0.1*i,alpha=0.3,**kwargs)
-  del data_frac
-
-ax.legend(fontsize=14)
-ax.set_xscale('log')
-ax.set_xlim(1e-1, 1e4)
-ax.set_xlabel(r'Einstein Crossing Time (days)',fontsize=20)
-ax.set_ylabel('Number of Events',fontsize=20)
-ax.xaxis.set_tick_params(labelsize=18)
-ax.yaxis.set_tick_params(labelsize=18)
-
-ax.set_axisbelow(True)
-ax.grid(linestyle=':', linewidth=0.5, color='k', which='major', alpha=0.6)
-
-fig.savefig('macho_discovery.pdf')
+plt.savefig('macho_discovery.pdf',bbox_inches='tight')
